2d58531abd82a948f990e12e15cac14f7930d0fd
Ravindra Yadav <yadavra2005@gmail.com>
2024-09-28 16:03:51 +0530

    learn making model and schema using mongoose and make server using express

---
+++ models/Ecommerce_modeling/user.mode.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("users", userSchema);
